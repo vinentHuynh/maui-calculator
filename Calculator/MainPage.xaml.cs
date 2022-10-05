9ff8ed4156d11f0fe455f5b91909532f367e61a1
Vincent <vincenthuynh02@gmail.com>
2022-10-04 21:28:07 -0500

    Revert "added parantheses, mod, and sqrt functionality"
    This reverts commit 6ed72eb7b92e42f31b6106991b4149caefd86ee8.

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -12,7 +12,12 @@ public partial class MainPage : ContentPage
 
     }
 
-
+    string currentEntry = "";
+    
+    string mathOperator;
+    
+    string decimalFormat = "N0";
+    
     
 
     void OnSelectNumber(object sender, EventArgs e)
@@ -21,8 +26,21 @@ public partial class MainPage : ContentPage
         Button button = (Button)sender;
         string pressed = button.Text;
 
-    
-    
+        currentEntry = pressed;
+
+        //if ((this.resultText.Text == "0" && pressed == "0")
+        //    || (currentEntry.Length <= 1 && pressed != "0")
+        //    || currentState < 0)
+        //{
+        //    this.resultText.Text = "";
+        //    if (currentState < 0)
+        //        currentState *= -1;
+        //}
+
+        //if (pressed == "." && decimalFormat != "N2")
+        //{
+        //    decimalFormat = "N2";
+        //}
 
         this.resultText.Text = pressed;
         this.CurrentCalculation.Text += pressed;
@@ -37,72 +55,103 @@ public partial class MainPage : ContentPage
         
         Button button = (Button)sender;
         string pressed = button.Text;
-
-        if (pressed == "sqrt")
-        {
-            pressed += '(';
-        }
-        this.CurrentCalculation.Text += pressed;
+        mathOperator = pressed;
+        this.CurrentCalculation.Text += mathOperator;
     }
 
-  
+    //private void LockNumberValue(string text)
+    //{
+    //    double number;
+    //    if (double.TryParse(text, out number))
+    //    {
+    //        if (currentState == 1)
+    //        {
+    //            firstNumber = number;
+    //        }
+    //        else
+    //        {
+    //            secondNumber = number;
+    //        }
+
+    //        currentEntry = string.Empty;
+    //    }
+    //}
+
     void OnClear(object sender, EventArgs e)
     {
-
+        //firstNumber = 0;
+        //secondNumber = 0;
+        //currentState = 1;
+        decimalFormat = "N0";
         this.resultText.Text = "0";
-
+        currentEntry = string.Empty;
         this.CurrentCalculation.Text = string.Empty;
     }
 
     void OnCalculate(object sender, EventArgs e)
     {
         
+                //LockNumberValue(resultText.Text);
+           DataTable dt = new DataTable();
 
-        DataTable dt = new DataTable();
+        // double result = Calculator.Calculate(firstNumber, secondNumber, mathOperator);
+
+        // this.CurrentCalculation.Text = $"{firstNumber} {mathOperator} {secondNumber}";
+
+        // this.resultText.Text = result.ToTrimmedString(decimalFormat);
+        // firstNumber = result;
+        // secondNumber = 0;
+        //currentState = -1;
         string expression = this.CurrentCalculation.Text;
         expression = expression.Replace('×', '*');
         expression = expression.Replace('÷', '/');
-        expression = expression.Replace("mod", "%");
         
-        if(expression.Contains("sqrt"))
-        {
-            int sqrt = expression.IndexOf("sqrt");
-            int endParanth = expression.IndexOf(")");
-            string substring = expression.Substring(sqrt + 5, endParanth-(sqrt+5));
-            var eval = (int)dt.Compute(substring, "");
-            double value = Math.Sqrt(eval);
-            
-            expression = expression.Remove(sqrt, endParanth-sqrt+1);
-            expression = expression.Insert(sqrt, value.ToString());
-            this.resultText.Text = dt.Compute(expression, "").ToString();
-        }
+        var result = (int)dt.Compute(expression, "");
         
-        else
-        {
-            this.resultText.Text = dt.Compute(expression, "").ToString();
-        }
-        
-        
-     
+        this.resultText.Text = result.ToString();
+        currentEntry = string.Empty;
         
     }    
 
     void OnNegative(object sender, EventArgs e)
     {
-        this.resultText.Text = (-1*Convert.ToDouble(this.resultText.Text)).ToString();
-
+        //if (currentState == 1)
+        //{
+        //    secondNumber = -1;
+        //    mathOperator = "×";
+        //    currentState = 2;
+        //    // OnCalculate(this, null);
+        //}
     }
 
     void OnPercentage(object sender, EventArgs e)
     {
-        double num = Convert.ToDouble(this.resultText.Text);
-        num = num / 100;
-        this.resultText.Text = num.ToString() + "%";
-       
-
+        //if (currentState == 1)
+        //{
+        //    LockNumberValue(resultText.Text);
+        //    decimalFormat = "N2";
+        //    secondNumber = 0.01;
+        //    mathOperator = "×";
+        //    currentState = 2;
+        //    OnCalculate(this, null);
+        //}
     }
+    void OnSqrt(object sender, EventArgs e)
+    {
+        //if(currentState ==1)
+        //{
 
-
+        //}
+    }
+    void OnLeftPara(object sender, EventArgs e)
+    {
+    }
+    void OnRightPara(object sender, EventArgs e)
+    {
+    }
+    void OnMod(object sender, EventArgs e)
+    {
+    }
 
 
 }
